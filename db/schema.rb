3c4ccc6ede6f9a017229e31643f8b8f3a49d9bb4
Kristina Markina <kr.markina@gmail.com>
2021-05-12 19:03:43 -0400

    added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210512215751) do
+ActiveRecord::Schema.define(version: 20210512225153) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
