531b027bcf555039175a64784f7be4a128c3cfcb
Kristina Markina <kr.markina@gmail.com>
2021-05-21 15:09:53 -0400

    3. Create an OrdersController. Add methods for index, show, new, edit, create, update, and destroy.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,74 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:edit, :update, :destroy]
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+    if Order.exists?(params[:id])
+      @order = Order.find(params[:id])
+    else
+      redirect_to orders_url
+    end
+  end
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash.notice = "The order record was created successfully."
+      redirect_to @order
+    else
+      flash.now.alert = @order.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
+  def update
+    if @order.update(customer_params)
+      flash.notice = "The customer record was updated successfully."
+      redirect_to @order
+    else
+      flash.now.alert = @order.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:product_name, :product_count, :customer)
+  end
 end
