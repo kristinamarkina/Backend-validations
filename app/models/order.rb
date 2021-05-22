65219c9af3691ce77d6be72c81351ebdf2f2c5a6
Kristina Markina <kr.markina@gmail.com>
2021-05-22 02:15:18 -0400

    8. Add validations to the Order model so that validates that a product_name, product_count, and customer_id are present, and so that it checks that the customer_id actually corresponds to a real customer record

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  validates :product_name, presence: true
+  validates :product_count, presence: true
+  validates :product_count, numericality: {only_integer: true}
 end
